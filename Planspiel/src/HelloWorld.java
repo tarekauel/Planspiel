fd032c9f31b2d16219d94631c779a81bfcb22061
Tarek Auel <tarek.auel@googlemail.com>
2013-08-29 13:20:23 +0200

    Fix

--- Planspiel/src/HelloWorld.java
+++ Planspiel/src/HelloWorld.java
@@ -7,10 +7,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hallo Gruppe");
 		System.out.println("Hier ist Felix!");
-<<<<<<< HEAD
-=======
 		System.out.println("Hier ist Tarek");
->>>>>>> parent of f77662a... HelloWorld changed
+
 
 	}
 
