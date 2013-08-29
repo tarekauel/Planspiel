f77662afd4d71510ee777ee777892e18e65b81b3
Lars Trey <lars.trey@gmail.com>
2013-08-29 13:14:45 +0200

    HelloWorld changed

--- Planspiel/src/HelloWorld.java
+++ Planspiel/src/HelloWorld.java
@@ -5,9 +5,10 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hallo Gruppe");
+		System.out.println("Hallo Gruppe!");
 		System.out.println("Hier ist Felix!");
-		System.out.println("Hier ist Tarek");
+		System.out.println("Hier ist Tarek!");
+		System.out.println("Hier ist Lars!");
 
 	}
 
