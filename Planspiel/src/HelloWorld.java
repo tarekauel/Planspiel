b44693f5b2f8fb782cbd6545e473a8a6832cb992
Lars Trey <lars.trey@gmail.com>
2013-08-29 13:18:12 +0200

    Revert "HelloWorld changed"
    This reverts commit f77662afd4d71510ee777ee777892e18e65b81b3.
    
    Conflicts:
    	Planspiel/src/HelloWorld.java

--- Planspiel/src/HelloWorld.java
+++ Planspiel/src/HelloWorld.java
@@ -7,6 +7,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hallo Gruppe");
 		System.out.println("Hier ist Felix!");
+<<<<<<< HEAD
+=======
+		System.out.println("Hier ist Tarek");
+>>>>>>> parent of f77662a... HelloWorld changed
 
 	}
 
