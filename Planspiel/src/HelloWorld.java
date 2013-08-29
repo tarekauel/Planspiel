1cd8a7da446c1d5b75cb7750165761b835c74f12
Michael.Scheid <michischeid@web.de>
2013-08-29 13:16:00 +0200

    Ich bin da

--- Planspiel/src/HelloWorld.java
+++ Planspiel/src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hallo Gruppe");
 		System.out.println("Hier ist Felix!");
+		System.out.println("Michael ist auch dabei!");
 
 	}
 
