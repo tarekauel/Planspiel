b54aa0d179fd58c20ff550e1f1b01035e3d85455
Lars Tilsner <lars.tilsner@googlemail.com>
2013-09-27 23:38:11 +0200

    update Purchase (Vererbung implementiert)

--- Planspiel/src/Server/Purchase.java
+++ Planspiel/src/Server/Purchase.java
@@ -1,66 +1,101 @@
 package Server;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * Die Purchase-Klasse stellt die Einkaufsabteilung des Unternehmens dar.
- * Sie kann an den beschaffungsmarkt neue Anfragen stellen und die Antworten
+ * Die Purchase-Klasse stellt die Einkaufsabteilung des Unternehmens dar. Sie
+ * kann an den beschaffungsmarkt neue Anfragen stellen und die Antworten
  * daraufhin mit der Angabe der gewuenschten Menge annehmen.
  * 
- * @author Felix 
- *
+ * @author Felix
+ * 
  */
 
+public class Purchase extends Department {
+/**
+ * Name wird vom Konstruktor gesetzt.
+ * @param c Company, Unternehmensreferenz
+ * @param fix FixKosten
+ * @throws Exception falls keine Korrekte erstellung
+ */
+	public Purchase(Company c, int fix) throws Exception {
+		super(c, "Einkauf", fix);
+	}
 
-public class Purchase {
+	/**
+	 * privater Konstruktor, dass niemand einen falschen Abteilungsnamen eingibt
+	 * 
+	 * @param c Company = Unternehmen
+	 * @param n Name = Abteilungsname
+	 * @param f fixCosts = Fix Kosten der Abteilung
+	 * @throws Exception falls die Abteilung nicht korrekt erstellt werden konnte
+	 */
+	private Purchase(Company c, String n, int f) throws Exception {
+		super(c, n, f);
+		// TODO Auto-generated constructor stub
+	}
 
 	private ArrayList<Request> listOfRequests = new ArrayList<Request>();
-	
+
 	/**
 	 * erstellt eine neue Anfrage an den Beschaffungsmarkt
-	 * @param resource: Für eine spezielle resource(Wafer oder Case inklusive Qualität)
-	 * @throws Exception: falls die Resource null ist
+	 * 
+	 * @param resource
+	 *            : Für eine spezielle resource(Wafer oder Case inklusive
+	 *            Qualität)
+	 * @throws Exception
+	 *             : falls die Resource null ist
 	 */
-	
-	public void createRequest(Resource resource) throws Exception{
-		
-		if(resource==null){
-			throw new NullPointerException("Resource is null! Class Purchase Method createRequest");
+
+	public void createRequest(Resource resource) throws Exception {
+
+		if (resource == null) {
+			throw new NullPointerException(
+					"Resource is null! Class Purchase Method createRequest");
 		}
-		
-		Request request = new Request(resource);
-		//Request request = Request.create(resource);
+
+		Request request = Request.create(resource);
+		// Request request = Request.create(resource);
 		listOfRequests.add(request);
-		
+
 	}
-	
+
 	/**
 	 * Die SupplierOffer wird unter Angabe eine gewuenschten Menge angenommen
-	 * @param supplierOffer: Offer auf die sich bezogen wird
-	 * @param quantity: Menge, die gewuenscht ist
-	 * @throws Exception: falls SupplierOffer null ist oder die Anzahl kleiner null.s
+	 * 
+	 * @param supplierOffer
+	 *            : Offer auf die sich bezogen wird
+	 * @param quantity
+	 *            : Menge, die gewuenscht ist
+	 * @throws Exception
+	 *             : falls SupplierOffer null ist oder die Anzahl kleiner null.s
 	 */
-	
-	public void acceptSupplierOffer(SupplierOffer supplierOffer, int quantity) throws Exception{
-		if(supplierOffer == null){
-			throw new NullPointerException("supplierOffer is null! Class Purchase Method acceptSupplierOffer");
+
+	public void acceptSupplierOffer(SupplierOffer supplierOffer, int quantity)
+			throws Exception {
+		if (supplierOffer == null) {
+			throw new NullPointerException(
+					"supplierOffer is null! Class Purchase Method acceptSupplierOffer");
 		}
-		
-		if(quantity<=0){
-			throw new IOException("Quantity is 0 or lower! Class Purchase Method acceptSupplierOffer");
+
+		if (quantity <= 0) {
+			throw new IOException(
+					"Quantity is 0 or lower! Class Purchase Method acceptSupplierOffer");
 		}
-		
+
 		Resource resource = supplierOffer.getResource();
-		if(resource == null){
-			throw new NullPointerException("resource of supplierOffer is null! Class Purchase Method acceptSupplierOffer");
+		if (resource == null) {
+			throw new NullPointerException(
+					"resource of supplierOffer is null! Class Purchase Method acceptSupplierOffer");
 		}
-		
-		Storage storage = company.getStorage();
+
+		Storage storage = this.getCompany().getStorage();
 		storage.store(resource, quantity);
 	}
-	
-	public ArrayList<Request> getListOfRequest(){
+
+	public ArrayList<Request> getListOfRequest() {
 		return listOfRequests;
-	}//getListOfRequest
-	
+	}// getListOfRequest
+
 }
