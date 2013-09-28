be4ae78fc1fbb804430a734bc1d163846956036e
Lars Trey <lars.trey@gmail.com>
2013-09-28 18:57:01 +0200

    Fixkosten als Konstruktor-Parameter hinzugefügt, um gleichartige Initialisierung von speziellen Abteilungen in der Company zu ermöglichen (Sinnvoll?)

--- Planspiel/src/Server/Production.java
+++ Planspiel/src/Server/Production.java
@@ -3,21 +3,21 @@ package Server;
 import java.util.ArrayList;
 
 public class Production extends Department {
-	// Liste aller jemals erstellten Produktions aufträge
+	// Liste aller jemals erstellten Produktions auftrï¿½ge
 	ArrayList<ProductionOrder> listOfAllProductionOrders;
-	// Liste aller offenen (noch nicht produzierten) Aufträgen
+	// Liste aller offenen (noch nicht produzierten) Auftrï¿½gen
 	ArrayList<ProductionOrder> listOfOpenProductionOrders;
-	// Referenz auf die Maschine (auf der wir ja produzieren müssen)
+	// Referenz auf die Maschine (auf der wir ja produzieren mï¿½ssen)
 	Machinery machine;
 
-	// Liste der Auslastungen für diesen Spieler
+	// Liste der Auslastungen fï¿½r diesen Spieler
 	ArrayList<TPercentOfUsage> listOfAllPercentOfUsage;
 
-	// Parameter für die Anzahl der Wafer pro Panel:
+	// Parameter fï¿½r die Anzahl der Wafer pro Panel:
 	private int waferPerPanel = 54; // 72 max, 36 min ~54 durchschnitt
 
 	/**
-	 * Regulärer Konstruktor der Produktion, erzeugt zeitgleich eine neue
+	 * Regulï¿½rer Konstruktor der Produktion, erzeugt zeitgleich eine neue
 	 * Maschine
 	 * 
 	 * @param c
@@ -27,8 +27,8 @@ public class Production extends Department {
 	 * @throws Exception
 	 *             falls Abteilung nicht erstellt werden konnte
 	 */
-	public Production(Company c) throws Exception {
-		super(c, "Produktion", 0);
+	public Production(Company c, int fix) throws Exception {
+		super(c, "Produktion", fix);
 		machine = new Machinery();
 	}
 	public int getFixCosts(){
@@ -55,7 +55,7 @@ public class Production extends Department {
 
 	/**
 	 * 
-	 * @return gibt die Maschine der Produktion zurück
+	 * @return gibt die Maschine der Produktion zurï¿½ck
 	 */
 	public Machinery getMachine() {
 		return this.machine;
@@ -67,47 +67,47 @@ public class Production extends Department {
 	 * @param wafer
 	 *            Der Wafer mit dem die Produktion gestartet wird
 	 * @param cases
-	 *            Das Gehäuse mit dem die Produktion gestartet wird
+	 *            Das Gehï¿½use mit dem die Produktion gestartet wird
 	 * @param quantity
-	 *            Die Anzahl der gewünschten Produktion
+	 *            Die Anzahl der gewï¿½nschten Produktion
 	 */
 	public void createProductionOrder(Resource wafer, Resource cases,
 			int quantity) {
 		// erzeuge den Auftrag:
 		ProductionOrder po = new ProductionOrder(wafer, cases, quantity);
-		// nimm ihn in die Liste ALLER Aufträge auf
+		// nimm ihn in die Liste ALLER Auftrï¿½ge auf
 		listOfAllProductionOrders.add(po);
-		// nimm ihn in die Liste der noch nicht bearbeiteten Aufträge auf
+		// nimm ihn in die Liste der noch nicht bearbeiteten Auftrï¿½ge auf
 		listOfOpenProductionOrders.add(po);
 	}
 
 	/**
-	 * produziert alle offenen Produktionsaufträge und leert die Liste der
-	 * offenen Aufträge am Ende. Das Abbuchen der Rohstoffe erfolgt in der
+	 * produziert alle offenen Produktionsauftrï¿½ge und leert die Liste der
+	 * offenen Auftrï¿½ge am Ende. Das Abbuchen der Rohstoffe erfolgt in der
 	 * Produktion, das gutschreiben der Panels im Auftrag
 	 * 
 	 */
 	public void produce() {
-		// Gibt die Maximale Anzahl der Werkstücke an
+		// Gibt die Maximale Anzahl der Werkstï¿½cke an
 		int max = this.machine.getMaxCapacity();
-		// Zählt mit, wieviele Werkstücke auf der Maschine lagen
+		// Zï¿½hlt mit, wieviele Werkstï¿½cke auf der Maschine lagen
 		int triedToProduce = 0;
 
-		// Es muss sicher gestellt werden, dass nicht mehr Werkstücke auf der
+		// Es muss sicher gestellt werden, dass nicht mehr Werkstï¿½cke auf der
 		// Maschine lagen, als diese kann.
 
-		// Schleife über alle Production Orders
+		// Schleife ï¿½ber alle Production Orders
 		for (int i = 0; i < listOfOpenProductionOrders.size(); i++) {
-			// Darf überhaupt noch jemand produzieren?
+			// Darf ï¿½berhaupt noch jemand produzieren?
 			if (triedToProduce >= max) {
 				// scheinbar nicht
 				break;
 			}
 
-			// Schleife über jede einzelne Position des Auftrages (Werkstück)
+			// Schleife ï¿½ber jede einzelne Position des Auftrages (Werkstï¿½ck)
 			for (int j = 0; j < listOfOpenProductionOrders.get(i)
 					.getRequested(); j++) {
-				// Darf überhaupt noch jemand produzieren?
+				// Darf ï¿½berhaupt noch jemand produzieren?
 				if (triedToProduce >= max) {
 					// scheinbar nicht
 					break;
@@ -115,33 +115,33 @@ public class Production extends Department {
 
 				// Abbuchen der Ressourcen:
 				// Zieh die Storage elemente aus dem Storage ab:
-				// nutze dafür das lager des spielers:
+				// nutze dafï¿½r das lager des spielers:
 				// Wafer abbuchen (direkt in der Anzahl waferPerPanel)
 				this.getCompany()
 						.getStorage()
 						.unstore(listOfOpenProductionOrders.get(i).getWafer(),
 								waferPerPanel);
-				// Gehäuse abbuchen
+				// Gehï¿½use abbuchen
 				this.getCompany()
 						.getStorage()
 						.unstore(listOfOpenProductionOrders.get(i).getCase(), 1);
 
-				// "Werkstück auf Maschine legen":
+				// "Werkstï¿½ck auf Maschine legen":
 				triedToProduce++;
-				// Gucken, ob wir tatsächlich produzieren:
+				// Gucken, ob wir tatsï¿½chlich produzieren:
 				if (machine.isJunk()) {
 					// Das Teil ist also Ausschuss
 					continue;
 				} else {
 					// Produziere das fertige Panel
-					// TODO: remove 100 , fester Wert für Motivation etc.
+					// TODO: remove 100 , fester Wert fï¿½r Motivation etc.
 					listOfOpenProductionOrders.get(i).produce(100,
 							this.getCompany().getStorage());
 				}
 			}
 		}
 
-		// summe aller aufträge
+		// summe aller auftrï¿½ge
 		int sum = 0;
 		// hole Summe aller ProductionOrders dieser Runde
 		for (ProductionOrder o : listOfOpenProductionOrders) {
@@ -155,15 +155,15 @@ public class Production extends Department {
 			// Fehler bei der Erstellung einer Auslastung
 			// TODO: Fehlerhandling?!
 		}
-		// Initialisiere die offenen Aufträge, damit keine Aufträge in die
-		// nächste Runde rutschen
+		// Initialisiere die offenen Auftrï¿½ge, damit keine Auftrï¿½ge in die
+		// nï¿½chste Runde rutschen
 		listOfOpenProductionOrders.clear();
 	}
 
 	/**
 	 * 
-	 * @return gibt die Liste aller bisher erstellten Produktionsaufträge seit
-	 *         Spielbeginn zurück
+	 * @return gibt die Liste aller bisher erstellten Produktionsauftrï¿½ge seit
+	 *         Spielbeginn zurï¿½ck
 	 */
 	public ArrayList<ProductionOrder> getListOfAllProductionOrders() {
 		return listOfAllProductionOrders;
@@ -171,7 +171,7 @@ public class Production extends Department {
 
 	/**
 	 * 
-	 * @return gibt die Liste aller offenen Aufträge zurück, also die, die diese
+	 * @return gibt die Liste aller offenen Auftrï¿½ge zurï¿½ck, also die, die diese
 	 *         Runde angelegt wurden
 	 */
 	public ArrayList<ProductionOrder> getListOfOpenProductionOrders() {
@@ -180,7 +180,7 @@ public class Production extends Department {
 
 	/**
 	 * 
-	 * @return gibt die Liste aller Auslastungen für diesen Spieler wieder
+	 * @return gibt die Liste aller Auslastungen fï¿½r diesen Spieler wieder
 	 */
 	public ArrayList<TPercentOfUsage> getListOfAllPercentOfUsage() {
 		return listOfAllPercentOfUsage;
