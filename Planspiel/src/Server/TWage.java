65aa60a83f18c7a69173b59fbc370d4b041a704b
Lars Tilsner <lars.tilsner@googlemail.com>
2013-09-29 14:39:01 +0200

    update

--- Planspiel/src/Server/TWage.java
+++ Planspiel/src/Server/TWage.java
@@ -11,11 +11,26 @@ public class TWage {
 
     private int amount;
     private int wageLevel;
-
-    public TWage(int amount) {
-        this.amount = amount;
+private int round;
+    
+    public TWage(int amount) throws Exception{
+        if(!checkAmount(amount)){
+        	throw new IllegalArgumentException ("Ung�ltiger Betrag");
+        }
+        if (checkRoundValid(round)==false){
+			//Runden check failed
+			throw new IllegalArgumentException("Round invalid");
+		}
     }
 
+    private boolean checkAmount(int amount) {
+		return (amount > 0);
+	}
+    private boolean checkRoundValid(int round){
+		
+		return (round>0);
+	}
+    
     private void calcWageLevel(){
         //Dazugehöriger Teil muss noch in GameEngine ausprogrammiert werden
         //Prinzip: Hole Gehälter aller Spieler aus HR und addiere
