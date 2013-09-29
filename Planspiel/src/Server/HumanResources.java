6dac2e0ce83fddadc25932b99734f7ec59c211b0
Lars Tilsner <lars.tilsner@googlemail.com>
2013-09-29 21:57:08 +0200

    Test

--- Planspiel/src/Server/HumanResources.java
+++ Planspiel/src/Server/HumanResources.java
@@ -1,54 +1,80 @@
 package Server;
 
+import java.util.ArrayList;
+
+import Logger.Log;
+
 /**
- * Created by:
- * User: Lars Trey
- * Date: 28.09.13
- * Time: 18:11
+ * Created by: User: Lars Trey Date: 28.09.13 Time: 18:11
  */
 
-public class HumanResources extends Department {
+public class HumanResources extends DepartmentRoundSensitive {
 
-    private TWage wagePerRound;                         //Lohn pro Runde pro Mitarbeiter
-    private int countEmployees;                         //Anzahl Mitarbeiter
-    private int wagesSum;                               //Gesamtkosten Löhne
-    private BenefitBooking benefitBooking;
+	private TWage wagePerRound; // Lohn pro Runde pro Mitarbeiter
+	private int countEmployees; // Anzahl Mitarbeiter
+	private int wagesSum; // Gesamtkosten Löhne
+	private ArrayList<BenefitBooking> benefitBooking;
+	private int workingHoursPerRound = 0;
 
-    public HumanResources(Company c, int fix) throws Exception {
-        super(c,"Personal",fix);
-        setCountEmployees(100);                         //TODO: Anpassen & in ini-File auslagern
-        setWagePerRound(new TWage(100,GameEngine.getGameEngine().getRound()));                //TODO: Anpassen & in ini-File auslagern
-        this.wagesSum = calcWagesSum();
-        this.benefitBooking = new BenefitBooking();
-        
-    }
+	public HumanResources(Company c, int fix) throws Exception {
+		super(c, "Personal", fix);
+		setCountEmployees(100); // TODO: Anpassen & in ini-File auslagern
+		setWagePerRound(new TWage(100, GameEngine.getGameEngine().getRound())); // TODO:
+																				// Anpassen
+																				// &
+																				// in
+																				// ini-File
+																				// auslagern
+		this.wagesSum = calcWagesSum();
+//TODO: BENEFIT BOOKING!?
+		//this.benefitBooking = new BenefitBooking();
 
-    private int calcWagesSum(){
-        return wagePerRound.getAmount() * this.countEmployees;
-    }
+	}
 
-    public void setWagePerRound(TWage wagePerRound) {
-        this.wagePerRound = wagePerRound;
-    }
+	private int calcWagesSum() {
+		return wagePerRound.getAmount() * this.countEmployees;
+	}
 
-    public void setCountEmployees(int countEmployees) {
-        this.countEmployees = countEmployees;
-    }
+	public void setWagePerRound(TWage wagePerRound) {
+		this.wagePerRound = wagePerRound;
+	}
 
-    public TWage getWagesPerHour() {
-        return wagePerRound;
-    }
+	public void setCountEmployees(int countEmployees) {
+		this.countEmployees = countEmployees;
+	}
 
-    public int getCountEmployees() {
-        return countEmployees;
-    }
+	public TWage getWagesPerHour() {
+		return wagePerRound;
+	}
 
-    public int getWagesSum() {
-        return wagesSum;
-    }
+	public int getCountEmployees() {
+		return countEmployees;
+	}
 
-    public BenefitBooking getBenefitBooking() {
-        return benefitBooking;
-    }
+	public int getWagesSum() {
+		return wagesSum;
+	}
+
+	public ArrayList<BenefitBooking> getBenefitBooking() {
+		
+		return benefitBooking;
+	}
+
+	public void increaseWorkingHour(int quantity) {
+		Log.method(quantity);
+		workingHoursPerRound++;
+	}
+
+	public int getWorkingHours() {
+		Log.get(workingHoursPerRound);
+		return workingHoursPerRound;
+	}
+
+	@Override
+	public void prepareForNewRound(int round) {
+		Log.method(round);
+		workingHoursPerRound = 0;
+
+	}
 
 }
