e95dc7b4244c06ff0cce1f3b6019593086f86379
Lars Tilsner <lars.tilsner@googlemail.com>
2013-09-29 14:41:39 +0200

    update

--- Planspiel/src/Server/TWage.java
+++ Planspiel/src/Server/TWage.java
@@ -1,46 +1,44 @@
 package Server;
 
 /**
- * Created by:
- * User: Lars Trey
- * Date: 28.09.13
- * Time: 19:04
+ * Created by: User: Lars Trey Date: 28.09.13 Time: 19:04
  */
 
 public class TWage {
 
-    private int amount;
-    private int wageLevel;
-private int round;
-    
-    public TWage(int amount) throws Exception{
-        if(!checkAmount(amount)){
-        	throw new IllegalArgumentException ("Ung�ltiger Betrag");
-        }
-        if (checkRoundValid(round)==false){
-			//Runden check failed
+	private int amount;
+	private int wageLevel;
+	private int round;
+
+	public TWage(int amount) throws Exception {
+		if (!checkAmount(amount)) {
+			throw new IllegalArgumentException("Ung�ltiger Betrag");
+		}
+		if (checkRoundValid(round) == false) {
+			// Runden check failed
 			throw new IllegalArgumentException("Round invalid");
 		}
-    }
+	}
 
-    private boolean checkAmount(int amount) {
+	private boolean checkAmount(int amount) {
 		return (amount > 0);
 	}
-    private boolean checkRoundValid(int round){
-		
-		return (round>0);
+
+	private boolean checkRoundValid(int round) {
+
+		return (round > 0);
 	}
-    
-    private void calcWageLevel(){
-        //Dazugehöriger Teil muss noch in GameEngine ausprogrammiert werden
-        //Prinzip: Hole Gehälter aller Spieler aus HR und addiere
-    }
 
-    public int getAmount() {
-        return amount;
-    }
+	private void calcWageLevel() {
+		// Dazugehöriger Teil muss noch in GameEngine ausprogrammiert werden
+		// Prinzip: Hole Gehälter aller Spieler aus HR und addiere
+	}
 
-    public int getWageLevel() {
-        return wageLevel;
-    }
+	public int getAmount() {
+		return amount;
+	}
+
+	public int getWageLevel() {
+		return wageLevel;
+	}
 }
--- Planspiel/src/Test/TypeTest.java
+++ Planspiel/src/Test/TypeTest.java
@@ -24,27 +24,27 @@ public class TypeTest {
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TPercentOfUsageInvalidRound() throws Exception {
 
-		 new TPercentOfUsage(500, -2);
+		new TPercentOfUsage(500, -2);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TPercentOfUsageRound0() throws Exception {
-		 new TPercentOfUsage(500, 0);
+		new TPercentOfUsage(500, 0);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TPercentOfUsageInvalidPercent() throws Exception {
 
-		 new TPercentOfUsage(-500, 2);
+		new TPercentOfUsage(-500, 2);
 
 	}
 
 	@Test
 	public void TPercentOfUsageValid() throws Exception {
 
-		 new TPercentOfUsage(50, 2);
+		new TPercentOfUsage(50, 2);
 
 	}
 
@@ -54,34 +54,34 @@ public class TypeTest {
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TPresentValueInvalidPresentValueAndRound() throws Exception {
 
-		 new TPresentValue(-5000, -2);
+		new TPresentValue(-5000, -2);
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TPresentValueInvalidRound() throws Exception {
 
-		 new TPresentValue(5000, -2);
+		new TPresentValue(5000, -2);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TPresentValueRound0() throws Exception {
 
-		 new TPresentValue(5000, 0);
+		new TPresentValue(5000, 0);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TPresentValueInvalidValue() throws Exception {
 
-		 new TPresentValue(-500, 2);
+		new TPresentValue(-500, 2);
 
 	}
 
 	@Test
 	public void TPresentValueValid() throws Exception {
 
-		 new TPresentValue(5000, 2);
+		new TPresentValue(5000, 2);
 
 	}
 
@@ -91,58 +91,57 @@ public class TypeTest {
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TResourcePriceInvalidLowQualityAndPrice() throws Exception {
 
-		 new TResourcePrice(-50, -200);
+		new TResourcePrice(-50, -200);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TResourcePriceInvalidHighQualityAndPrice() throws Exception {
 
-		 new TResourcePrice(101, -200);
+		new TResourcePrice(101, -200);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TResourcePriceInvalidPrice() throws Exception {
 
-
-		 new TResourcePrice(50, -20);
+		new TResourcePrice(50, -20);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TResourcePricePrice0() throws Exception {
-		 new TResourcePrice(50, 0);
+		new TResourcePrice(50, 0);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TResourcePriceQuality0() throws Exception {
-		 new TResourcePrice(0, 1500);
+		new TResourcePrice(0, 1500);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TResourcePriceQuality0AndPrice0() throws Exception {
-		 new TResourcePrice(0, 0);
+		new TResourcePrice(0, 0);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TResourcePriceInvalidLowQuality() throws Exception {
-		 new TResourcePrice(-50, 2);
+		new TResourcePrice(-50, 2);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TResourcePriceInvalidHighQuality() throws Exception {
-		 new TResourcePrice(101, 2);
+		new TResourcePrice(101, 2);
 
 	}
 
 	@Test
 	public void TResourcePriceValid() throws Exception {
-		 new TResourcePrice(50, 200);
+		new TResourcePrice(50, 200);
 
 	}
 
@@ -152,57 +151,57 @@ public class TypeTest {
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TMotivationInvalidLowMotivationAndRound() throws Exception {
 
-		 new TMotivation(-50, -200);
+		new TMotivation(-50, -200);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TMotivationInvalidHighMotivationAndRound() throws Exception {
 
-		 new TMotivation(101, -200);
+		new TMotivation(101, -200);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TMotivationInvalidRound() throws Exception {
 
-		 new TMotivation(50, -20);
+		new TMotivation(50, -20);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TMotivationRound0() throws Exception {
-		 new TMotivation(50, 0);
+		new TMotivation(50, 0);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TMotivationMotivation0() throws Exception {
-		 new TMotivation(0, 2);
+		new TMotivation(0, 2);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TMotivationMotivation0AndRound0() throws Exception {
-		 new TMotivation(0, 0);
+		new TMotivation(0, 0);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TMotivationInvalidLowMotivation() throws Exception {
-		 new TMotivation(-50, 2);
+		new TMotivation(-50, 2);
 
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void TMotivationInvalidHighMotivation() throws Exception {
-		 new TMotivation(10000, 2);
+		new TMotivation(10000, 2);
 
 	}
 
 	@Test
 	public void TMotivationValid() throws Exception {
-		 new TMotivation(100, 2);
+		new TMotivation(100, 2);
 
 	}
 
