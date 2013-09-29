8a2c8700daf7c4556d3a09d1157488dc14e054ac
Lars Tilsner <lars.tilsner@googlemail.com>
2013-09-29 13:01:32 +0200

    Update

--- Planspiel/src/Server/Location.java
+++ Planspiel/src/Server/Location.java
@@ -1,37 +1,87 @@
 package Server;
 
+import java.util.*;
+
 /**
- * Created by:
- * User: Lars Trey
- * Date: 28.09.13
- * Time: 17:16
+ * Created by: User: Lars Trey Date: 28.09.13 Time: 17:16
  */
 
 public class Location {
 
-    private int advantage;
-    private String country;
-    private int purchasePrice;
-    private int wageLevel;
+	private int advantage;
+	private String country;
+	private int purchasePrice;
+	private int wageLevel;
+	private ArrayList<Location> listOfLocations;
 
-    public Location(){
+	/**
+	 * muss vor Spielstart aufgerufen werden. Erzeugt die verschiedenen
+	 * Locations
+	 */
+	public static void initLocations() {
+		new Location(120,"Deutschland",60000000,9164);
+		new Location(110,"USA",40000000,8054);
+		new Location(90,"China",20000000,6175);
+		new Location(80,"Indien",5000000,3092);
+		
+	}
 
-    }
+	/**
+	 * Erzeugt eine neue Location
+	 * 
+	 * @param a
+	 *            Advantage/ Standortvorteil
+	 * @param c
+	 *            Country / Produktionsstandort
+	 * @param p
+	 *            Price /Anschaffungskosten des Standorts
+	 * @param w
+	 *            WageLevel / Lohn niveau
+	 */
+	public Location(int a, String c, int p, int w) {
+		this.advantage = a;
+		this.country = c;
+		this.purchasePrice = p;
+		this.wageLevel = w;
+		
+		listOfLocations.add(this);	
+		
+	}
 
-    public int getAdvantage(){
-        return this.advantage;
-    }
+	public ArrayList<Location> getListOfLocations() {
+		return listOfLocations;
+	}
+	/**
+	 * Sucht nach einer Location über das Land
+	 * @param c Country: das zu suchende Land
+	 * @return null, falls der Name nicht gefunden wurde
+	 */
+	public Location getLocationByCountry(String c){
+		//Suche in der internen Liste:
+		for(Location o:listOfLocations){
+			//Vergleiche (ignoriere Groß/Kleinschreibung)
+			if(o.getCountry().toLowerCase().equals(c.toLowerCase())){
+				//Strings stimmen überein, also zurückgeben
+				return o;
+			}
+		}
+		return null;
+	}
 
-    public String getCountry(){
-        return this.country;
-    }
+	public int getAdvantage() {
+		return this.advantage;
+	}
 
-    public int getPurchasePrice(){
-        return this.purchasePrice;
-    }
+	public String getCountry() {
+		return this.country;
+	}
 
-    public int getWageLevel(){
-        return this.wageLevel;
-    }
+	public int getPurchasePrice() {
+		return this.purchasePrice;
+	}
+
+	public int getWageLevel() {
+		return this.wageLevel;
+	}
 
 }
