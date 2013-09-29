36bbdd4ecfdd0d04e53712811e798283ba57dc3f
Lars Tilsner <lars.tilsner@googlemail.com>
2013-09-29 14:55:31 +0200

    update

--- Planspiel/src/Server/HumanResources.java
+++ Planspiel/src/Server/HumanResources.java
@@ -20,6 +20,7 @@ public class HumanResources extends Department {
         setWagePerRound(new TWage(100,GameEngine.getGameEngine().getRound()));                //TODO: Anpassen & in ini-File auslagern
         this.wagesSum = calcWagesSum();
         this.benefitBooking = new BenefitBooking();
+        
     }
 
     private int calcWagesSum(){
