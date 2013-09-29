1a21394f782e37539d37b28f3cc2c6851a46d582
Lars Tilsner <lars.tilsner@googlemail.com>
2013-09-29 18:18:40 +0200

    TestTemplate

---
+++ Planspiel/src/Test/TestTemplate.java
@@ -0,0 +1,29 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+import Server.*;
+
+public class TestTemplate {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+	}
+
+	@Before
+	public void createMachinery() {
+	}
+
+	@Test
+	public void initializeTest() {
+	}
+
+		
+	@After
+	public void resetMachine() {
+		
+	}
+
+}
