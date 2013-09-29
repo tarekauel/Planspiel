4efb1474f57751439d6a20315b420613b55ac99e
Lars Tilsner <lars.tilsner@googlemail.com>
2013-09-29 13:20:38 +0200

    update

--- Planspiel/src/Server/Company.java
+++ Planspiel/src/Server/Company.java
@@ -1,72 +1,69 @@
 package Server;
 
 /**
- * Created by:
- * User: Lars Trey
- * Date: 28.09.13
- * Time: 17:22
+ * Created by: User: Lars Trey Date: 28.09.13 Time: 17:22
  */
 
 public class Company {
 
-    private Location location;
-    private BankAccount bankAccount;
+	private Location location;
+	private BankAccount bankAccount;
 
-    private Purchase purchase;
-    private Production production;
-    private Storage storage;
-    private Distribution distribution;
-    private HumanResources humanResources;
-    private MarketResearch marketResearch;
+	private Purchase purchase;
+	private Production production;
+	private Storage storage;
+	private Distribution distribution;
+	private HumanResources humanResources;
+	private MarketResearch marketResearch;
 
-	public Company()throws Exception {
-
-        this.location = new Location();
-        this.bankAccount = new BankAccount(0); //TODO: Startwert des Vermögens setzen
-        
-        
-        this.purchase = new Purchase(this, 1000000);
-        this.production = new Production(this, 1000000);
-        this.storage = new Storage(this, 1000000);
-        this.distribution = new Distribution(this, 1000000);
-        this.humanResources = new HumanResources(this, 1000000);
-        this.marketResearch = new MarketResearch(this, 1000000);
+	public Company(Location l) throws Exception {
+		//erzeuge Bankkonto mit 1 Mio Kapital
+		bankAccount = new BankAccount(100000000);
+		// setze Location
+		this.location = l;
+		// 'Kaufe die Location'
+		bankAccount.decreaseBalance(l.getPurchasePrice());
+		
+		//Erzuege alle Abteilungen
+		this.purchase = new Purchase(this, 1000000);
+		this.production = new Production(this, 1000000);
+		this.storage = new Storage(this, 1000000);
+		this.distribution = new Distribution(this, 1000000);
+		this.humanResources = new HumanResources(this, 1000000);
+		this.marketResearch = new MarketResearch(this, 1000000);
 
 	}
 
-  
 	public Location getLocation() {
 		return this.location;
 	}
-
 
 	public BankAccount getBankAccount() {
 		return this.bankAccount;
 	}
 
+	public Purchase getPurchase() {
+		return this.purchase;
+	}
 
-	public Purchase getPurchase(){
-        return this.purchase;
-    }
+	public Production getProduction() {
+		return this.production;
+	}
 
-    public Production getProduction(){
-        return this.production;
-    }
+	public Storage getStorage() {
+		return this.storage;
+	}
 
-    public Storage getStorage(){
-        return this.storage;
-    }
+	public Distribution getDistribution() {
+		return this.distribution;
+	}
 
-    public Distribution getDistribution(){
-        return this.distribution;
-    }
+	public HumanResources getHumanResources() {
+		return this.humanResources;
+	}
 
-    public HumanResources getHumanResources(){
-        return this.humanResources;
-    }
+	public MarketResearch getMarketResearch() {
+		return this.marketResearch;
+	}
 
-    public MarketResearch getMarketResearch(){
-        return this.marketResearch;
-    }
-	
 }
