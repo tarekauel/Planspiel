acecbbea71c3732d3bca68c8b01e35a9d7056070
Lars Tilsner <lars.tilsner@googlemail.com>
2013-09-29 17:45:03 +0200

    Logging

--- Planspiel/src/Server/Company.java
+++ Planspiel/src/Server/Company.java
@@ -1,5 +1,7 @@
 package Server;
 
+import Logger.Log;
+
 /**
  * Created by: User: Lars Trey Date: 28.09.13 Time: 17:22
  */
@@ -17,14 +19,15 @@ public class Company {
 	private MarketResearch marketResearch;
 
 	public Company(Location l) throws Exception {
-		//erzeuge Bankkonto mit 1 Mio Kapital
+		Log.newObj(l);
+		// erzeuge Bankkonto mit 1 Mio Kapital
 		bankAccount = new BankAccount(100000000);
 		// setze Location
 		this.location = l;
 		// 'Kaufe die Location'
 		bankAccount.decreaseBalance(l.getPurchasePrice());
-		
-		//Erzuege alle Abteilungen
+
+		// Erzuege alle Abteilungen
 		this.purchase = new Purchase(this, 1000000);
 		this.production = new Production(this, 1000000);
 		this.storage = new Storage(this, 1000000);
@@ -33,48 +36,64 @@ public class Company {
 		this.marketResearch = new MarketResearch(this, 1000000);
 
 	}
-	
-	public void initRound(int round){
+
+	public void initRound(int round) {
+		Log.method(round);
+		
 		purchase.prepareForNewRound(round);
 		production.prepareForNewRound(round);
 		storage.prepareForNewRound(round);
 		distribution.prepareForNewRound(round);
 		humanResources.prepareForNewRound(round);
 		marketResearch.prepareForNewRound(round);
+		Log.methodExit();
 		
 	}
-	
-	
+
 	public Location getLocation() {
+		Log.get(location);
 		return this.location;
 	}
 
 	public BankAccount getBankAccount() {
+		Log.get(bankAccount);
 		return this.bankAccount;
 	}
 
 	public Purchase getPurchase() {
+		Log.get(purchase);
 		return this.purchase;
 	}
 
 	public Production getProduction() {
+		Log.get(production);
 		return this.production;
 	}
 
 	public Storage getStorage() {
+
+		Log.get(storage);
 		return this.storage;
 	}
 
 	public Distribution getDistribution() {
+		Log.get(distribution);
 		return this.distribution;
 	}
 
 	public HumanResources getHumanResources() {
+
+		Log.get(humanResources);
 		return this.humanResources;
 	}
 
 	public MarketResearch getMarketResearch() {
+		Log.get(marketResearch);
 		return this.marketResearch;
 	}
-
+@Override
+	public String toString(){
+		return "Unternehmen in:" + this.location;
+		
+	}
 }
