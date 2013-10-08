9cfab933a6bfd606a6f6797b86b85903bc732093
felix(home) <Felix@.(none)>
2013-10-08 15:12:21 +0200

    update TestProduce

--- Planspiel/tests/ProductionTest/TestProduce.java
+++ Planspiel/tests/ProductionTest/TestProduce.java
@@ -12,12 +12,13 @@ import org.junit.Test;
 import Server.*;
 
 public class TestProduce {
-	
+
 	Company c;
 	Storage st;
-	Resource wafer;
+	Resource wafer80;
+	Resource wafer70;
 	Resource cases;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		Location.initLocations();
@@ -27,42 +28,68 @@ public class TestProduce {
 	public void initializeTests() throws Exception {
 		c = new Company(Location.getLocationByCountry("USA"));
 		st = c.getStorage();
-		wafer = new Resource(80, "Wafer", 100000);
+		wafer80 = new Resource(80, "Wafer", 100000);
+		wafer70 = new Resource(70, "Wafer", 100000);
 		cases = new Resource(80, "Gehäuse", 100000);
-		
+
 	}
 
-	@Test //so wird aber nicht die logik geprueft oder?
+	@Test
+	// so wird aber nicht die logik geprueft oder?
 	public void produceValid() throws Exception {
-		c.getStorage().store(wafer, 1080);
-		c.getStorage().store(cases, 20);
-		c.getProduction().createProductionOrder(wafer, cases, 10);
+		c.getStorage().store(wafer80, 2160);
+		c.getStorage().store(cases, 50);
+		c.getProduction().createProductionOrder(wafer80, cases, 40);
 		c.getProduction().produce();
-		assertEquals(1,st.getAllFinishedGoods().size());
+		assertEquals(1, st.getAllFinishedGoods().size());
 	}
-	
+
 	@Test
 	public void produceWithoutProdOrder() throws Exception {
 		c.getProduction().produce();
-		assertEquals(0,st.getAllFinishedGoods().size());
+		assertEquals(0, st.getAllFinishedGoods().size());
 	}
-	
+
 	@Test
 	public void produceMaxCapacityReached() throws Exception {
-		c.getStorage().store(wafer, 600);
+		c.getStorage().store(wafer80, 600);
+		c.getStorage().store(wafer70, 600);
 		c.getStorage().store(cases, 50);
-		c.getProduction().createProductionOrder(wafer, cases, 500);
-		c.getProduction().createProductionOrder(wafer, cases, 50);
+		// erstelle ProductionOrder die Kapazitaet der Maschine vollausreizt
+		// damit zweite ProductionOrder nicht produziert wird
+		c.getProduction().createProductionOrder(wafer80, cases, 501);
+		c.getProduction().createProductionOrder(wafer70, cases, 50);
 		c.getProduction().produce();
-		int quantity = 0;
-		ArrayList<StorageElement> arrayListSE = st.getAllStorageElements();
-		for(StorageElement storageElem: arrayListSE){
-			Product prod = storageElem.getProduct();
-			if(prod instanceof FinishedGood){
-				quantity = storageElem.getQuantity();
-			}
-		}
-		assertEquals(true,quantity == 500);
+		// pruefe ob zweite ProductionOrder nicht mehr produziert wurde
+		assertEquals(0, c.getProduction().getListOfAllProductionOrders().get(1)
+				.getProduced()
+				+ c.getProduction().getListOfAllProductionOrders().get(1)
+						.getWaste());
+
+	}
+
+	@Test
+	public void produceNotEnougWaferResources() throws Exception {
+		c.getStorage().store(wafer80, 1);
+		c.getStorage().store(cases, 50);
+		c.getProduction().createProductionOrder(wafer80, cases, 400);
+		c.getProduction().produce();
+		assertEquals(0, c.getProduction().getListOfAllProductionOrders().get(0)
+				.getProduced()
+				+ c.getProduction().getListOfAllProductionOrders().get(0)
+						.getWaste());
+	}
+
+	@Test
+	public void produceNotEnougCaseResources() throws Exception {
+		c.getStorage().store(wafer80, 500);
+		c.getStorage().store(cases, 1);
+		c.getProduction().createProductionOrder(wafer80, cases, 400);
+		c.getProduction().produce();
+		assertEquals(1, c.getProduction().getListOfAllProductionOrders().get(0)
+				.getWaste()
+				+ c.getProduction().getListOfAllProductionOrders().get(0)
+						.getProduced());
 	}
 
 	@After
