2a5eec845c52f253c980a76208a32a95abc835e5
felix(home) <Felix@.(none)>
2013-10-08 13:46:46 +0200

    TestProduce

--- Planspiel/tests/ProductionTest/TestProduce.java
+++ Planspiel/tests/ProductionTest/TestProduce.java
@@ -2,6 +2,8 @@ package ProductionTest;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -26,22 +28,41 @@ public class TestProduce {
 		c = new Company(Location.getLocationByCountry("USA"));
 		st = c.getStorage();
 		wafer = new Resource(80, "Wafer", 100000);
-		c.getStorage().store(wafer, 200);
 		cases = new Resource(80, "Gehäuse", 100000);
-		c.getStorage().store(cases, 200);
+		
 	}
 
-	@Test
+	@Test //so wird aber nicht die logik geprueft oder?
 	public void produceValid() throws Exception {
-		c.getProduction().createProductionOrder(wafer, cases, 5);
+		c.getStorage().store(wafer, 1080);
+		c.getStorage().store(cases, 20);
+		c.getProduction().createProductionOrder(wafer, cases, 10);
 		c.getProduction().produce();
-		assertEquals(0,st.getAllFinishedGoods().size());
+		assertEquals(1,st.getAllFinishedGoods().size());
 	}
 	
 	@Test
 	public void produceWithoutProdOrder() throws Exception {
 		c.getProduction().produce();
 		assertEquals(0,st.getAllFinishedGoods().size());
+	}
+	
+	@Test
+	public void produceMaxCapacityReached() throws Exception {
+		c.getStorage().store(wafer, 600);
+		c.getStorage().store(cases, 50);
+		c.getProduction().createProductionOrder(wafer, cases, 500);
+		c.getProduction().createProductionOrder(wafer, cases, 50);
+		c.getProduction().produce();
+		int quantity = 0;
+		ArrayList<StorageElement> arrayListSE = st.getAllStorageElements();
+		for(StorageElement storageElem: arrayListSE){
+			Product prod = storageElem.getProduct();
+			if(prod instanceof FinishedGood){
+				quantity = storageElem.getQuantity();
+			}
+		}
+		assertEquals(true,quantity == 500);
 	}
 
 	@After
