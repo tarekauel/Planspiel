90436ae6fd5fc807e65750219455466517f7f761
Michael.Scheid <michischeid@web.de>
2013-10-11 13:05:05 +0200

    update

--- Planspiel/src/Server/GameDataTranslator.java
+++ Planspiel/src/Server/GameDataTranslator.java
@@ -25,8 +25,6 @@ import Message.GameDataMessageToClient.PurchaseToClient.RequestToClient;
 import Message.GameDataMessageToClient.PurchaseToClient.RequestToClient.SupplierOfferToClient;
 import Message.GameDataMessageToClient.ReportingToClient;
 import Message.GameDataMessageToClient.ReportingToClient.FixCostToClient;
-import Message.GameDataMessageToClient.StorageToClient;
-import Message.GameDataMessageToClient.StorageToClient.StorageElenmentToClient;
 
 public class GameDataTranslator {
 
@@ -276,25 +274,13 @@ public class GameDataTranslator {
 		HumanResourcesToClient humanResources = createHumanResources(company);
 		MarketingToClient marketing = createMarketing(company);
 		ReportingToClient reporting = createReporting(company);
-		StorageToClient storage = createStorage(company);
 		//Hauptdaten erstellen
 		long cash = company.getBankAccount().getBankBalance();
 		long maxCredit = Constant.BankAccount.MAX_CREDIT;
 		
 		// Message erstellen
-		GameDataMessageToClient message = new GameDataMessageToClient(playerName, purchase, production, storage, distribution, humanResources, marketing, reporting, cash, maxCredit);
+		GameDataMessageToClient message = new GameDataMessageToClient(playerName, purchase, production, distribution, humanResources, marketing, reporting, cash, maxCredit);
 		return message;
-	}
-
-	private StorageToClient createStorage(Company company) {
-		Storage serverStorage = company.getStorage();
-		ArrayList<StorageElenmentToClient> storageElements = new ArrayList<StorageElenmentToClient>();
-		for (StorageElement storageElenment : serverStorage.getAllStorageElements()) {
-			storageElements.add(new StorageElenmentToClient(storageElenment.getProduct().getName(), storageElenment.getProduct().getQuality(), storageElenment.getQuantity()));
-		}
-		
-		StorageToClient storage = new StorageToClient(Constant.Product.STORAGECOST_WAFER, Constant.Product.STORAGECOST_CASE, Constant.Product.STORAGECOST_PANEL, storageElements);
-		return storage;
 	}
 
 	private ReportingToClient createReporting(Company company) {
@@ -410,7 +396,7 @@ public class GameDataTranslator {
 				.getListOfAllProductionOrders()) {
 			orders.add(new ProductionOrderToClient(productionOrder.getWafer()
 					.getQuality(), productionOrder.getCase().getQuality(),
-					productionOrder.getProduced(),productionOrder.getCase().getCosts()));
+					productionOrder.getProduced()));
 		}
 		ProductionToClient production = new ProductionToClient(orders);
 		return production;
