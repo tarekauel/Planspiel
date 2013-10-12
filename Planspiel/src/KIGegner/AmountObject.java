f91be7f44544432862a431b810ce24f66e54ec1f
Lars Tilsner <lars.tilsner@googlemail.com>
2013-10-13 00:53:15 +0200

    update der toString

--- Planspiel/src/KIGegner/AmountObject.java
+++ Planspiel/src/KIGegner/AmountObject.java
@@ -3,14 +3,23 @@ package KIGegner;
 public class AmountObject {
 	public final long amount;
 	public final int round;
-	public AmountObject(long amount, int round){
+
+	public AmountObject(long amount, int round) {
 		this.round = round;
 		this.amount = amount;
 	}
-	
+
 	@Override
-	public String toString(){
-		return "Runde:" + round + " - Guthaben:" + amount;
+	public String toString() {
+		if (round < 10) {
+			return "Runde:000" + round + " - Guthaben:" + amount;
+		} else if (round < 100) {
+			return "Runde:00" + round + " - Guthaben:" + amount;
+		} else if (round < 1000) {
+			return "Runde:0" + round + " - Guthaben:" + amount;
+		} else {
+			return "Runde:" + round + " - Guthaben:" + amount;
+		}
 	}
-	
+
 }
