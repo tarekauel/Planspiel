72f311e70f99d4b90c6ee3a1fc78881807e43716
Felix Kampsen <kampsen.felix@gmail.com>
2013-10-13 17:56:47 +0200

    update

--- Planspiel/tests/GameDataTranslatorToClient/D2MProduction.java
+++ Planspiel/tests/GameDataTranslatorToClient/D2MProduction.java
@@ -21,6 +21,9 @@ public class D2MProduction {
 	
 	Company c;
 	int acceptedSupplierOfferQuality;
+	int panelQuality;
+	int producedQuantity;
+	int costs;
 	
 	
 
@@ -36,19 +39,31 @@ public class D2MProduction {
 	public void initializeTests() throws Exception {
 		c = new Company(Location.getLocationByCountry("USA"),"OTTO");
 		Resource wafer = new Resource(80, "Wafer", 500);
+		c.getStorage().store(wafer, 10000);
 		Resource cases = new Resource(50, "Gehäuse", 10000);
-		c.getProduction().createProductionOrder(wafer, cases, 100)
+		c.getStorage().store(cases, 1000);
+		c.getProduction().createProductionOrder(wafer, cases, 100);
+		c.getProduction().produce();
+		panelQuality = c.getStorage().getAllFinishedGoods().get(0).getQuality();
+		producedQuantity = c.getProduction().getListOfAllProductionOrders().get(0).getProduced();
+		costs =  c.getProduction().getListOfAllProductionOrders().get(0).getPanel().getCosts();
+		
 		
 		
 		
 	}
 
 	@Test
-	public void getRightName() throws Exception {
+	public void getCompleteProductionOrder() throws Exception {
 		
 		 ArrayList<GameDataMessageToClient> gameDataMessageToClients = GameDataTranslator.getGameDataTranslator().createGameDataMessages();
-		 assertEquals(80,gameDataMessageToClients.get(0).purchase.requests.get(0).quality);
-		 assertEquals(acceptedSupplierOfferQuality,gameDataMessageToClients.get(0).purchase.requests.get(0).supplierOffers.get(0).quality);
+		 assertEquals(100,gameDataMessageToClients.get(0).production.orders.get(0).quantity);
+		 assertEquals(80,gameDataMessageToClients.get(0).production.orders.get(0).qualityWafer);
+		 assertEquals(50,gameDataMessageToClients.get(0).production.orders.get(0).qualityCase);
+		 assertEquals(panelQuality,gameDataMessageToClients.get(0).production.orders.get(0).qualityPanel);
+		 assertEquals(producedQuantity,gameDataMessageToClients.get(0).production.orders.get(0).producedQuantity);
+		 assertEquals(costs,gameDataMessageToClients.get(0).production.orders.get(0).costs);
+		
 
 	}
 
