5743c5a05d5d42aac4d40c5584b7e7c0d81ff7f8
Felix Kampsen <kampsen.felix@gmail.com>
2013-10-13 15:16:20 +0200

    update

--- Planspiel/tests/GameDataTranslator/Xyz.java
+++ Planspiel/tests/GameDataTranslator/Xyz.java
@@ -19,74 +19,67 @@ import Message.GameDataMessageFromClient.HumanResourcesFromClient.BenefitBooking
 import Message.GameDataMessageFromClient.ProductionFromClient.ProductionOrderFromClient;
 import Message.GameDataMessageFromClient.PurchaseFromClient.AcceptedSupplierOfferFromClient;
 import Message.GameDataMessageFromClient.PurchaseFromClient.RequestFromClient;
+import Message.GameDataMessageToClient.ReportingToClient.FixCostToClient;
+import Message.GameDataMessageToClient;
 import Server.Company;
 import Server.GameEngine;
 import Server.Location;
 
-public class M2DMarketResearch {
+public class Xyz {
 
-static Company c;
-	
+	static Company c;
+
 	PurchaseFromClient purchase;
 	DistributionFromClient distribution;
 	ProductionFromClient production;
-	HumanResourcesFromClient  hr;
-	
+	HumanResourcesFromClient hr;
+	ArrayList<GameDataMessageFromClient> gameDataMessages = new ArrayList<GameDataMessageFromClient>();
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		Location.initLocations();
 		GameEngine.getGameEngine();
-		c = new Company(Location.getLocationByCountry("USA"),"OTTO");
-		
-		
+		c = new Company(Location.getLocationByCountry("USA"), "OTTO");
+
 	}
 
 	@Before
 	public void initializeTests() throws Exception {
 
-	
-		//werden teilweise absichtlich leer gelassen da in anderem Test geprueft
+		// werden teilweise absichtlich leer gelassen da in anderem Test
+		// geprueft
 		ArrayList<RequestFromClient> requests = new ArrayList<RequestFromClient>();
 		ArrayList<AcceptedSupplierOfferFromClient> acceptedSupplierOffers = new ArrayList<AcceptedSupplierOfferFromClient>();
 		ArrayList<ProductionOrderFromClient> orders = new ArrayList<ProductionOrderFromClient>();
 		ArrayList<OfferFromClient> offers = new ArrayList<OfferFromClient>();
 		ArrayList<BenefitBookingFromClient> benefits = new ArrayList<BenefitBookingFromClient>();
-		
-		//weitere Erstellung der Message
-		 purchase = new PurchaseFromClient(requests, acceptedSupplierOffers);
-		 production = new ProductionFromClient(orders);
-		 distribution = new DistributionFromClient(offers);
-		 hr = new HumanResourcesFromClient(benefits);
-		
 
-		
-	}
+		// weitere Erstellung der Message
+		purchase = new PurchaseFromClient(requests, acceptedSupplierOffers);
+		production = new ProductionFromClient(orders);
+		distribution = new DistributionFromClient(offers);
+		hr = new HumanResourcesFromClient(benefits);
 
-	@Test
-	public void marketResearchOrdered() throws Exception {
-		boolean orderMarketResearch = true;
-		ArrayList<GameDataMessageFromClient> gameDataMessages = new ArrayList<GameDataMessageFromClient>();
-		GameDataMessageFromClient gameDataMessage = new GameDataMessageFromClient(c.getName(), purchase, production, distribution, false, hr, 8, orderMarketResearch);
+		GameDataMessageFromClient gameDataMessage = new GameDataMessageFromClient(c.getName(), purchase, production, distribution, false, hr, 7, false);
 		gameDataMessages.add(gameDataMessage);
-		
-		GameEngine.getGameEngine().startNextRound(gameDataMessages);
-		System.out.println(c.getMarketResearch().getIsBooked());
-		
-		
-	}
-	
-	@Test
-	public void marketResearchNotOrdered() throws Exception {
-		
-		
+
 	}
 
+	@Test
+	public void increaseMachineryLevel() throws Exception {
+		
+		ArrayList<GameDataMessageToClient> gameDataM2Client= GameEngine.getGameEngine().startNextRound(gameDataMessages);
+		GameDataMessageToClient toClient = gameDataM2Client.get(0);
+		double sum = 0;
+		for(FixCostToClient fix : toClient.reporting.fixCosts){
+			sum = sum + fix.costs;
+		}
+		System.out.println(sum);
+
+	}
 
 	@After
 	public void resetTests() {
 
 	}
-
-	
-	
 }
