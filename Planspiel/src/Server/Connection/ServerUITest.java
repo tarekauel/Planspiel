29270d259d86fadac515d1679872c1f4f4b6f68a
Felix Kampsen <kampsen.felix@gmail.com>
2013-10-14 16:58:58 +0200

    commit

--- Planspiel/src/Server/Connection/ServerUITest.java
+++ Planspiel/src/Server/Connection/ServerUITest.java
@@ -1,6 +1,14 @@
 package Server.Connection;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import Constant.Constant;
 import KIGegner.KITarek;
+import Message.GameDataMessageFromClient;
+import Message.GameDataMessageToClient;
+import Server.GameEngine;
+import Server.Location;
 
 /**
  * 
@@ -8,6 +16,12 @@ import KIGegner.KITarek;
  *         Server her.
  */
 public class ServerUITest  {
+	private static ServerUITest server;
+	private ArrayList<Player> playerList = new ArrayList<Player>();
+	private ConnectionListener connectionListener = null;
+	private int receivedGameMessages = 0;
+	private int maxPlayer = Constant.Server.MAX_PLAYER;
+
 	/**
 	 * startet den Server
 	 * 
